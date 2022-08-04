628a991687063601b4c7140163a529d5b33a6af5
WNC12k <40601544+WNC12k@users.noreply.github.com>
2022-08-04 22:52:03 +0500

    Nomad will be get enhanced Pistols depend of Rank Level

--- DoomRPG/scripts/Map.c
+++ DoomRPG/scripts/Map.c
@@ -395,6 +395,48 @@ NamedScript Type_OPEN void MapInit()
         if (CurrentLevel->Event == MAPEVENT_NONE)
             CallACS("jjirandomizer");
 
+    // Compatibility Handling - DoomRL Arsenal Extended
+    // Nomad - Give enhanced Pistol for Rank
+    if (CompatModeEx == COMPAT_DRLAX)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (!PlayerInGame(i)) continue;
+
+            if (PlayerClass(i) != 7) continue;
+
+            SetActivator(Players(i).TID);
+
+            if (Player.RankLevel >= 2)
+            {
+                // Take Pistol
+                TakeInventory(ItemData[0][1].Actor, 1);
+
+                // Take tokens from DoomRL Arsenal
+                RemoveDRLAItem(0, 1);
+
+                // Give enchanced Pistol
+                if (Player.RankLevel >= 12)
+                    GiveInventory("RLHighPowerMarksmanPistol", 1);
+                else if (Player.RankLevel >= 10)
+                    GiveInventory("RLHighPowerHandCannon", 1);
+                else if (Player.RankLevel >= 8)
+                    GiveInventory("RLHighPowerCombatPistol", 1);
+                else if (Player.RankLevel >= 6)
+                    GiveInventory("RLMarksmanPistol", 1);
+                else if (Player.RankLevel >= 4)
+                    GiveInventory("RLHandCannon", 1);
+                else if (Player.RankLevel >= 2)
+                    GiveInventory("RLCombatPistol", 1);
+
+                // Give token from DoomRL Arsenal
+                GiveInventory("RLWeaponLimit", 1);
+            }
+
+            SetActivator(0, AAPTR_NULL);
+        }
+    }
+
     CurrentLevel->Init = true;
 }
 
@@ -950,8 +992,18 @@ NumberedScript(MAP_EXIT_SCRIPTNUM) MapSpecial void MapExit(bool Secret, bool Tel
         PreviousPrimaryLevelNum = CurrentLevel->LevelNum;
 
     // Compatibility Handling - DoomRL Arsenal Extended
-    if (CompatModeEx == COMPAT_DRLAX && PlayerClass(PlayerNumber()) == 7 && Player.NomadLuckBonus < 100 * MapLevelModifier) // Nomad
-        Player.NomadLuckBonus = 100 * MapLevelModifier;
+    // Nomad - Increased Luck Stat for every Level completed
+    if (CompatModeEx == COMPAT_DRLAX)
+    {
+        for (int i = 0; i < MAX_PLAYERS; i++)
+        {
+            if (!PlayerInGame(i)) continue;
+
+            // Increased Luck Stat for Nomad Players
+            if (PlayerClass(i) == 7 && Players(i).NomadLuckBonus < 100 * MapLevelModifier)
+                Players(i).NomadLuckBonus = 100 * MapLevelModifier;
+        }
+    }
 
     // Exits
     if (CurrentLevel->UACArena)
