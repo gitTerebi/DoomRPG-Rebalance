afecc4e2562b3d05f8438555ba3897360cdbf2d3
somtam <tim@ocher.co>
2022-10-09 07:14:34 +0700

    disable astyle

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-Utilities\astyle\bin\astyle.exe  --style=allman  --recursive  DoomRPG/scripts/*.c,*.h
+::Utilities\astyle\bin\astyle.exe  --style=allman  --recursive  DoomRPG/scripts/*.c,*.h
 Utilities\python\python.exe compile.py 
 del IR\*.obj
